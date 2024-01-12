c3c8c8003778b4356a0cf4b64b619322ae1738d4
Mystery135 <websiteemail12345678910@gmail.com>
2024-01-12 00:35:37 -0700

    a

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function showStarInfo(){
-alert("sidjofp");
+function showStarInfo(info){
+let infoPanel = document.querySelector("#star-info-panel");
+infoPanel.classList.toggle("visible");
+}
+function hideStarInfo(){
+
+    let infoPanel = document.querySelector("#star-info-panel");
+    infoPanel.classList.toggle("visible");
+    
 
 }
+
+document.addEventListener('click', function(event) {
+    let panel = document.querySelector("#star-info-panel");
+    let isClickInsidePanel = panel.contains(event.target) || event.target.classList.contains("star");
+
+    if (!isClickInsidePanel && panel.classList.contains('visible')) {
+        panel.classList.remove('visible');
+    }
+});
