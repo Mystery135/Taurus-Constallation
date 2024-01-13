64ac0b82648e493345af0f3ff99b60e0fd94c6cf
Mystery135 <websiteemail12345678910@gmail.com>
2024-01-13 10:57:05 -0800

    Finished

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-function showStarInfo(info){
-let infoPanel = document.querySelector("#star-info-panel");
-infoPanel.classList.toggle("visible");
+function showStarInfo(name, picture, distance, radius, mass, amagnitude, abmagnitude, lumin, spectraltype, evo, age, info, showTable) {
+    let infoPanel = document.querySelector("#star-info-panel");
+    infoPanel.classList.toggle("visible");
+
+    document.querySelector("#star-name").textContent = name;
+
+    document.querySelector("#panel-img").src = picture;
+
+
+    document.querySelector("#table-star-name").textContent = name;
+
+    document.querySelector("#table-distance").textContent = distance + " light years";
+
+    document.querySelector("#table-radius").innerHTML = radius + " R<sub>☉</sub>";
+
+    document.querySelector("#table-mass").innerHTML = mass + " M<sub>☉</sub>";
+
+    document.querySelector("#table-apparent-magnitude").textContent = amagnitude;
+
+    document.querySelector("#table-absolute-magnitude").textContent = abmagnitude;
+
+    document.querySelector("#table-luminosity").textContent = lumin;
+
+    document.querySelector("#table-spectral-type").textContent = spectraltype;
+
+    document.querySelector("#table-evo").textContent = evo;
+
+    document.querySelector("#table-age").textContent = age;
+
+    document.querySelector("#star-info").textContent = info;
+
+    if (showTable == false){
+        document.querySelector("#star-table").style.display = 'none';
+    }else{
+        document.querySelector("#star-table").style.display = 'block';
+    }
+
+
+
 }
+
+
+
 function hideStarInfo(){
 
     let infoPanel = document.querySelector("#star-info-panel");
@@ -18,3 +57,4 @@ document.addEventListener('click', function(event) {
         panel.classList.remove('visible');
     }
 });
+
